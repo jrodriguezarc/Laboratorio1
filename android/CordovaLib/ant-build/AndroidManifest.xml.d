cc87d7343b7d19bc7b8f311c582327e93b319358
jrodriguezarc <peche_rodriguez@hotmail.com>
2014-08-24 18:32:15 +0200

    Nuevos cambios

---
+++ android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\JuanPC\Desktop\cr.tec.moviles\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\Users\JuanPC\Desktop\cr.tec.moviles\platforms\android\CordovaLib\AndroidManifest.xml \
---
+++ android/ant-build/HelloWorld-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+C:\Users\JuanPC\Desktop\cr.tec.moviles\platforms\android\ant-build\HelloWorld-debug-unaligned.apk : \
+C:\Users\JuanPC\Desktop\cr.tec.moviles\platforms\android\ant-build\HelloWorld.ap_ \
+C:\Users\JuanPC\Desktop\cr.tec.moviles\platforms\android\ant-build\classes.dex \
+C:\Users\JuanPC\Desktop\cr.tec.moviles\platforms\android\CordovaLib\ant-build\classes.jar \
